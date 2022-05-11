65fb0f83d7751959f50e421ad70b30ee2f88c133
knh6269 <44117975+knh6269@users.noreply.github.com>
2022-05-11 11:32:06 +0900

    Create Is Subsequence.js
    05.11 update DP

---
+++ Leetcode/Easy/Is Subsequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isSubsequence(s, t) {
+    let i=0;
+    let count=0;
+    while(i<s.length){
+         for(let val of t){
+            if(s[i]==val){
+                t=t.split("").splice(t.indexOf(val)+1,t.length+1).join("");
+                count++;
+                break;
+            }
+        }
+        i++;
+    }
+    if(count==s.length)return true;
+    else return false;
+};
